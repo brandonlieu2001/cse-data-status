698d4c06f332a69e805224f7fe7d63d3edb6d53c
brandonlieu2001 <brandonlieu2001@gmail.com>
2025-10-03 15:32:23 -0700

    Minority candidate calculations and annotations

--- missingness_summary.R
+++ missingness_summary.R
@@ -3,7 +3,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
-### Calculate totals per state ###
+# ----------------------------------------------------------Calculate number candidates and missingness per state spreadsheet ----------------------------------------------
+
 # find pathways to local files
 file_pathways <- list.files("/Users/brandonlieu/Desktop/sheriff_data_check", full.names = TRUE)
 
@@ -59,20 +60,20 @@ state_summaries <- lapply(state_sheets, function(state_data) {
     )
 })
 
-### Calculate totals across ALL states ###
-
+# ---------------------------------------------------------- Calculate total candidates/elections and missingness across ALL states using state_summaries -----------------------------------------------------------
 desc_vars <- c("n_candidates", "n_elections", "n_contested_elections", "n_missing_votes", "n_missing_gender", "n_missing_pronoun", "n_missing_ethnicity", 
           "n_missing_political", "n_missing_race", "n_missing_birthyear")
 
 grand_totals_df <- bind_rows(state_summaries) %>%
-  summarise(across(all_of(desc_vars), sum, na.rm = TRUE))
+  summarise(across(all_of(desc_vars), sum, na.rm = TRUE)) # applies sum to all_of the descriptive vars described above in entire dataset (all states)
 
 # Calculate proportions of missingness with grand totals across all states
 vars_prop <- c("n_missing_votes", "n_missing_gender", "n_missing_pronoun", "n_missing_ethnicity",
                "n_missing_political", "n_missing_race", "n_missing_birthyear")
 grand_total_prop_df <- grand_totals_df[vars_prop] / grand_totals_df[["n_candidates"]]
 
-### Visualization of missing data for entire data set ###
+# ---------------------------------------------------------- Visualization of missing data for entire data set ----------------------------
+
 grand_total_prop_df %>%
   pivot_longer(everything(), names_to = "variable", values_to = "prop") %>%
   ggplot(aes(x = prop*100, y = reorder(variable, prop))) +
@@ -85,3 +86,35 @@ grand_total_prop_df %>%
   ) +
   theme_minimal()
 
+# ---------------------------------------------------------- Minority candidate calculation ----------------------------------------------------------
+all_state_data <- bind_rows(state_sheets) %>% # combine all separate sheets onto one main sheet
+  filter(!(is.na(Statefips))) %>% # remove blank rows (no state associated)
+  filter(!is.na(`Candidate(winner/runner-up)_Firstname`) & `Candidate(winner/runner-up)_Firstname`!= "") # only include rows with candidates
+
+# Add one new col that detects if the candidate is minority
+all_state_data <- all_state_data %>% 
+  mutate(minority_flag = as.numeric((`Race: White` == FALSE) &
+    (`Race: Black or African American` == TRUE |
+       `Race: American Indian or Alaska Native` == TRUE |
+       `Race: Asian` == TRUE |
+       `Race: Native Hawaiian or Other Pacific Islander` == TRUE))
+    )
+
+# Total number of contested elections with at least 1 minority candidate 
+contest_elections_minority_summary <- all_state_data %>%
+  mutate(Election_Year_num = as.numeric(Election_Year),
+         contested_flag = if_else(as.numeric(Candidate_Index) == 2, 1, 0)) %>%
+  group_by(State_Name, County_Name, Election_Year_num) %>%
+  mutate(contested_flag = max(contested_flag)) %>%   # Set all rows in county-year to 1 if any county-year has contested_flag == 1
+  filter(contested_flag == 1) %>% # filter county-year only contested elections (n=413)
+  summarise(
+    n_minority = sum(minority_flag == 1, na.rm = TRUE), # given contested county-year, identify number of minorities involved
+    n_contested_elections = 1,                             # each county-year = 1 contested election
+    n_contested_elections_w_minority = as.integer(n_minority > 0) # given contested count-year, 1 = minority involved in contested, 0 otherwise
+  ) %>%
+  ungroup() %>% # disregard county-year grouping
+  summarise( 
+    total_minority = sum(n_minority), # total minorities involved in contested elections
+    total_contested = sum(n_contested_elections), # total contested elections
+    total_contested_w_minority = sum(n_contested_elections_w_minority) # total contested elections involving at least one minority
+  )
